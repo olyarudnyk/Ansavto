60cbc264ec3924b9ad71e3b0f0ba8034ecc8e93a
OlgaRudnyk <grigoryanolga97@gmail.com>
2021-08-25 03:39:49 +0300

    add calc form sliders

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ for (const arrow of arrows) {
     page.classList.toggle('header-inner-carousel-moved');
   })
 }
+
+
+const calcButtons = document.querySelectorAll('.calc-slider-btn');
+const calcForms = document.querySelectorAll('.calc-leasing');
+
+for (const button of calcButtons) {
+  button.addEventListener('click', function() {
+    for (const b of calcButtons) {
+      if (button === b) {
+        b.classList.add('active')
+      } else {
+        b.classList.remove('active')
+      }
+    }
+
+    const buttonFormName = button.getAttribute('data-form')
+    for (const form of calcForms) {
+      const formName = form.getAttribute('data-form')
+      if (buttonFormName === formName) {
+        form.classList.add('active') 
+      } else {
+        form.classList.remove('active')
+      }
+
+    }
+
+  })
+}
